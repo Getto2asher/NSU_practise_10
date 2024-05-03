440c6a473481d7b3ea733874c35bfa83d4e8103d
Getto2asher <109324161+Getto2asher@users.noreply.github.com>
2024-05-03 23:32:03 +0700

    1

--- 7/solution.py
+++ 7/solution.py
@@ -1,5 +1,8 @@
-with open('input.txt', 'a+') as file:
+with open('input.txt') as file:
     data = list(map(str.strip, file.readlines()))
+
+with open('input.txt', 'w') as file1:
     for el in data:
-        file.write(el)
-        file.write('\n')
+        if el != '100':
+            file1.write(el)
+            file1.write('\n')
